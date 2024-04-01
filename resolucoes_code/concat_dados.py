5a1817c49d15bf7cb8901d4cb2a1d6499ce62034
Sabrina Alves <96206813+sabrinalves01@users.noreply.github.com>
2024-04-01 08:39:30 -0300

    Update concat_dados.py

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,13 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+# Resolução
+
+# Recebendo os dados do usuário
+dado1 = input("Digite o primeiro dado: ")
+dado2 = input("Digite o segundo dado: ")
+
+# Concatenando os dados em uma única string
+concatenado = dado1 + dado2
+
+# Exibindo o resultado
+print("Os dados concatenados são:", concatenado)
